75a1115f2c8553b2761aba67581c4a5ffb885778
Curtis Hildebrand <38567578+kti-curtis@users.noreply.github.com>
2019-02-05 13:50:06 -0800

    type change

--- convertCAD.js
+++ convertCAD.js
@@ -2,7 +2,7 @@ function convertToUSD(event) {
   if ( event.target.value > 1 || event.target.value < 100 ) {
     var change = document.getElementById("equivUSD");
     console.log("event.target.value: ", event.target.value);
-    change.innerHTML = event.target.value;
+    change.innerHTML = "<p>" + event.target.value + "</p>";
   } else {
     var cadvalid = document.getElementById("CADvalidation");
     cadvalid.innerHTML = "Enter a number between 1 and 100";
